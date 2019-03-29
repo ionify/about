2b320be162c67f6a5de837479fd77ad267cac3dd
Mike Lee <iskitz@users.noreply.github.com>
2019-03-29 04:21:55 -0700

    docs: team: updated member & moment terms

--- team/team.js
+++ team/team.js
@@ -1,29 +1,29 @@
 ;
 
 ~
-{ id: "team.0.1.2017.05.30-07@ionify"
+{ id: "team.0.1-7.20190329@ionify"
     ,
   is:
     [ "A team inventing optimized, natural interfaces, for you. We aspire to create "
     + "accessible, simple, performant, inspiring & reliable experiences for everyone."
     
-    , {creator: "mike.lee@ionify"
-      ,     at: "2007.09-04"
+    , {created: "mike.lee@ionify"
+      ,     on: "2007.09-04"
       ,     in: "forest-hills.new-york.usa.earth"
       }
       
-    , { author: "2nd.team.member@domain"
-      ,     at: "YEAR[.MM[.DD]][+|-TimezoneHour.MinuteOffset]"
+    , { edited: "2nd.team.member@domain"
+      ,     on: "YEAR[.MM[.DD]][+|-TimezoneHour.MinuteOffset]"
       ,     in: "[town.][city.][state.][country.]planet"
       }
 
-    , { tester: "next.ascending.order.team.member@domain"
-      ,     at: "YEAR[.MM[.DD]][+|-TimezoneHour.MinuteOffset]"
+    , { tested: "next.ascending.order.team.member@domain"
+      ,     on: "YEAR[.MM[.DD]][+|-TimezoneHour.MinuteOffset]"
       ,     in: "[town.][city.][state.][country.]planet"
       }
       
-    , { writer: "newest.team.member@domain"
-      ,     at: "2017.05.30-07"
+    , {trained: "newest.team.member@domain"
+      ,     on: "2017.05.30-07"
       ,     in: "san-jose.california.usa.earth"
       }
     ]
