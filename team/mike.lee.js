9f9eda645eaa28168ae83ded1b504b3878887d03
Mike Lee <iskitz@users.noreply.github.com>
2020-04-27 09:07:49 -0700

    code: identity: updated mike.lee & team

--- team/mike.lee.js
+++ team/mike.lee.js
@@ -1,33 +1,35 @@
 ;
 ~
 { re:
-    { id: 'mike.lee'
-    , by: 'mike.lee'
-    , at: 'ionify.net'
+    { id:  'mike.lee'
+    , as: ['person.0', 'ion']
+    , by:  'mike.lee@ionify.net'
+    , at: -0.010
     , on: -7.20180327
-    , to: -8.20190421
-    , is: +1.07
-    , it: "identifies Mike Lee"
+    , to: -7.20200427
+    , is: "Mike Lee's Identifiers"
     }
     ,
   is:
-    [ 'Michael Lee'        , 'Mike Lee'
-    , 'mike.lee@iskitz'    , 'mike.lee@ionify'
-    , 'mike.lee@ionified'  , 'mikeleepro@linkedin'
-    , 'iskitz@github'      , 'iskitz@glitch'
-    , 'iskitz@sourceforge' , 'iskitz@mdn'
-    , 'iskitz@trello'      , 'iskitz@twitter'
-    , 'iskitz@yahoo'       , 'iskitz@repl.it'
-    , 'iskitzdev@yahoo'    , 'mike-lee@jsperf'
-    , 'mikeizworkin@yahoo' , 'mikeizworkin@github'
-    , 'mike.iskitz.net'
+    [ 'Michael Lee'         , 'Mike Lee'
+    , 'mike.lee@ionify'     , 'mike.lee@ionified'
+    , 'mike.lee@iskitz'     , 'mikeleepro@linkedin'
+    , 'iskitz@dev.to'       , 'iskitz@github'
+    , 'iskitz@glitch'       , 'iskitz@mdn'
+    , 'iskitz@sourceforge'  , 'iskitz@trello'
+    , 'iskitz@twitter'      , 'iskitz@repl.it'
+    , 'iskitz@yahoo'        , 'iskitzdev@yahoo'
+    , 'mike-lee@jsperf'     , 'mikeizworkin@github'
+    , 'mikeizworkin@yahoo'  , 'mike.iskitz.net'
     ]
     ,
   creator:
-    [       'ionified',  'ionify',  'ion',     'jsonxd',   'ajile'
-    ,        'ask.say', 'wakatta', 'jeni',   'anemojii'
-    ,  'webOS 3.x Calendar Fixes', 'webOS 3.x Calendar', 'DataHub'
-    ,  'hollyhouk.com', 'MEAP Remote Management System'
+    [ 'ionizer', 'ionix', 'ionified', 'ionify', 'jsonxd', 'ajile'
+    , 'ask.say', 'wakatta', 'anemojii'
+    , 'aeon', 'aesop', 'dion', 'ion', 'jem', 'jeni', 'jet', 'jewel', 'lion'
+    , 'obi', 'oj', 'orion', 'sasi' , 'storie', 'webi'
+    , 'webOS 3.x Calendar Fixes', 'webOS 3.x Calendar', 'DataHub'
+    , 'hollyhouk.com', 'MEAP Remote Management System'
     , 'iDesign@iskitz'
     ]
     ,
@@ -36,10 +38,10 @@
     ]
     ,
   member:
-    [ 'netflix', 'devcolor', 'khanacademy'
-    , 'mozilla',   'amazon', 'hp',  'palm'
-    ,   'canon',   'sybase', 'nsbe', 'wpi'
-    , 'inroads',    'emsep', 'strive'
+    [ 'netflix', '/dev/color', 'khanacademy'
+    , 'mozilla',     'amazon',  'hp', 'palm'
+    ,   'canon',     'sybase', 'bsu', 'nsbe'
+    ,     'wpi',    'inroads', 'emsep', 'strive'
     , 'brooklyn technical high school'
     , 'philippa schuyler', 'foster-laurie'
     , 'st. agnes'
--- team/team.js
+++ team/team.js
@@ -1,32 +1,57 @@
 ;
-
 ~
-{ id: "team.0.1-7.20190329@ionify"
-    ,
-  is:
+{ id:  'team@ionify'
+, as: ['team.0', 'ion']
+, at: -0.01
+, on: -4.200709
+, to: -7.20200427
+, is:
     [ "A team inventing optimized, natural interfaces, for you. We aspire to create "
     + "accessible, simple, performant, inspiring & reliable experiences for everyone."
-    
-    , {created: "mike.lee@ionify"
-      ,     on: "2007.09-04"
-      ,     in: "forest-hills.new-york.usa.earth"
-      }
-      
-    , { edited: "2nd.team.member@domain"
-      ,     on: "YEAR[.MM[.DD]][+|-TimezoneHour.MinuteOffset]"
-      ,     in: "[town.][city.][state.][country.]planet"
-      }
+    ,
 
-    , { tested: "next.ascending.order.team.member@domain"
-      ,     on: "YEAR[.MM[.DD]][+|-TimezoneHour.MinuteOffset]"
-      ,     in: "[town.][city.][state.][country.]planet"
-      }
-      
-    , {trained: "newest.team.member@domain"
-      ,     on: "2017.05.30-07"
-      ,     in: "san-jose.california.usa.earth"
+      {creator: 'mike.lee@ionify'
+      ,     of: ['ionify', 'ionify.about','ionified', 'ionix', 'ionizer'
+                ]
+      ,     on: -4.200709
+      ,     to: -7.20200427
+      ,     in: 'forest-hills.new-york.usa.earth'
+      },
+
+      {creator: 'moses.mccall@ionify'
+      ,     of: ['ionizer']
+      ,     on: -7.20200422
+      ,     to: -7.20200427
+      ,     in: 'denver.colorado.usa.earth'
+      },
+
+      {editor: 'jeb.walton@ionify'
+      ,     of: ['ionify.about']
+      ,     on: -7.20200421
+      ,     to: -7.20200427
+      ,     in: 'connecticut.usa.earth'
+      },
+
+      { editor: "2nd.team.member@domain"
+      ,     of: ['...']
+      ,     on: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' |0
+      ,     to: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' |0
+      ,     in: '[hyphenated-town.][hyphenated-city.][hyphenated-state.][hyphenated-country.]planet'
+      },
+
+      { tester: "next.ascending.order.team.member@domain"
+      ,     of: ['...']
+      ,     on: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' |0
+      ,     to: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' |0
+      ,     in: '[hyphenated-town.][hyphenated-city.][hyphenated-state.][hyphenated-country.]planet'
+      },
+
+      {trainer: "newest.team.member@domain"
+      ,     of: ['...']
+      ,     on: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' |0
+      ,     to: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' |0
+      ,     in: '[hyphenated-town.][hyphenated-city.][hyphenated-state.][hyphenated-country.]planet'
       }
     ]
 }
-
 ;
