5ca028a451d473e298f4389642f2dd885307d466
Mike Lee <iskitz@users.noreply.github.com>
2020-04-28 03:53:42 -0700

    docs & code: stories: + actual code for about page's code samples

---
+++ ions/stories.js
@@ -0,0 +1,54 @@
+;
+~
+{ id: 'stories@ions.iskitz.net'
+, by: 'mike.lee@ionify.net'
+, in: 'san-jose.california.us.earth'
+, on: -7.202003210313
+, to: -7.202003210349
+, at: -0.1
+, as: 'storie'
+, is:
+    [ `an exploration of prose as
+       stories: sentence template
+       or
+       regular-ion-expressions`
+    ]
+, it:
+    [ `shows that prose can be
+       written as code via stories
+       joined by trailing /'s
+       which enables increased
+       readability & avoids issues
+       with stories being parsed
+       as c-style single-line
+       comments!`
+    ]
+, we:
+    [ `must ...`
+    , `will ...`
+    , `want ...`
+    , `love ...`
+    , `like ...`
+    , `know ...`
+    , `wish ...`
+    , `cant ...`
+    , `wont ...`
+    ]
+, do:
+    / This shows how prose can  //
+    / be written as code via    //
+    / stories joined by         //
+    / trailing \/'s. It shows   //
+    / how javascript's          //
+    / regular-expression        //
+    / literals support this &   //
+    / some of their syntactic   //
+    / limitatons like needing   //
+    / to know & escape a set of //
+    / special characters &      //
+    / symbols; some of which    //
+    / are common when writing   //
+    / spoken languages like     //
+    / English.                   /
+}
+;
---
+++ ions/xml.stories.js
@@ -0,0 +1,55 @@
+;
+~
+{ id:  'xml.stories@ions.iskitz.net'
+, by:  'mike.lee@ionify.net'
+, in:  'san-jose.california.us.earth'
+, on:  -7.202003200700
+, to:  -7.202003210407
+, at:  -0.4
+, as: ['xml','stories']
+, is:  `an exploration of xml as
+        stories`
+, it: [`shows that xml can be
+        transformed to and
+        ingested as stories by:`
+      ,`inserting $'s between
+        xml tags, and /'s before
+        each tag's name & closing
+        > bracket!`
+      ,`replacing leading <'s &
+        trailing >'s with /'s!`
+      ]
+, we: [`must ...`
+      ,`will ...`
+      ,`want ...`
+      ,`love ...`
+      ,`like this for non-web js environments like node.js`
+      ,`know that web views can already import xml to js`
+      ,`wish ...`
+      ,`cant ...`
+      ,`wont ...`
+      ]
+, do: $=1
+}
+
+$ </these/>
+$   </look like=xml/>
+$ </or/>
+$   </xhtml/>
+$ </or/>
+$   </html data-attribute=🤓/>
+$   </tags/>
+$ </but/>
+$ </these are=ions😉 by=👨🏾‍💻/>
+$
+
+; /these//
+     /look somewhat like=xml//
+  /or//
+     /xhtml//
+  /or//
+     /html data-attribute=🤓//
+     /tags//
+  /but//
+  /these are=stories😉 by=👨🏾‍💻/
+;
