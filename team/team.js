2d91aea6f0678e321bd58f56625f32b6a04fb483
Mike Lee <iskitz@users.noreply.github.com>
2020-04-28 03:31:31 -0700

    data: ionify: team: refined description, structure & member details

--- team/team.js
+++ team/team.js
@@ -1,55 +1,63 @@
 ;
 ~
-{ id:  'team@ionify'
-, as: ['team.0', 'ion']
-, at: -0.01
-, on: -4.200709
-, to: -7.20200427
-, is:
-    [ "A team inventing optimized, natural interfaces, for you. We aspire to create "
-    + "accessible, simple, performant, inspiring & reliable experiences for everyone."
-    ,
+{ re:
+    { id:  'team@ionify.net'
+    , as: ['team.0']
+    , at: -0.02
+    , on: -4.200709
+    , to: -7.20200428
+    , is: "a team inventing optimized natural interfaces for you, to introduce"
+        + " novel, simple, performant, intuitive & reliable experiences."
+    },
 
-      {creator: 'mike.lee@ionify'
-      ,     of: ['ionify', 'ionify.about','ionified', 'ionix', 'ionizer'
+  team:
+    [ {creator: 'mike.lee@ionify'
+      ,     of: [ 'ionify', 'ionify.about', 'ionified', 'ionix', 'ionizer'
+                , 'aeon'  , 'aesop',  'ai', 'dion'    , 'im'   , 'ion'
+                , 'jem'   , 'jeni' , 'jet', 'jewel'   , 'lion' , 'obi'
+                , 'oj'    , 'orion','sasi', 'storie'  , 'webi'
                 ]
       ,     on: -4.200709
-      ,     to: -7.20200427
-      ,     in: 'forest-hills.new-york.usa.earth'
+      ,     to: -7.20200428
+      ,     in: [        'forest-hills.new-york.usa.earth'
+                , 'springfield-gardens.new-york.usa.earth'
+                ,          'san-jose.california.usa.earth'
+                ,                      'tokyo.japan.earth'
+                ]
       },
 
       {creator: 'moses.mccall@ionify'
       ,     of: ['ionizer']
       ,     on: -7.20200422
-      ,     to: -7.20200427
+      ,     to: -7.20200426
       ,     in: 'denver.colorado.usa.earth'
       },
 
-      {editor: 'jeb.walton@ionify'
+      {editor: 'jeb.walton@gmail'
       ,     of: ['ionify.about']
       ,     on: -7.20200421
-      ,     to: -7.20200427
+      ,     to: -7.20200422
       ,     in: 'connecticut.usa.earth'
       },
 
-      { editor: "2nd.team.member@domain"
+      { editor: "next.descending.order.team.member@domain"
       ,     of: ['...']
-      ,     on: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' |0
-      ,     to: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' |0
+      ,     on: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' *1
+      ,     to: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' *1
       ,     in: '[hyphenated-town.][hyphenated-city.][hyphenated-state.][hyphenated-country.]planet'
       },
 
-      { tester: "next.ascending.order.team.member@domain"
+      { tester: "next.descending.order.team.member@domain"
       ,     of: ['...']
-      ,     on: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' |0
-      ,     to: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' |0
+      ,     on: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' *1
+      ,     to: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' *1
       ,     in: '[hyphenated-town.][hyphenated-city.][hyphenated-state.][hyphenated-country.]planet'
       },
 
-      {trainer: "newest.team.member@domain"
+      {trainer: "next.descending.order.team.member@domain"
       ,     of: ['...']
-      ,     on: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' |0
-      ,     to: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' |0
+      ,     on: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' *1
+      ,     to: '+|-TimezoneHour[MinuteOffset].YEAR[.MM[.DD]]' *1
       ,     in: '[hyphenated-town.][hyphenated-city.][hyphenated-state.][hyphenated-country.]planet'
       }
     ]
