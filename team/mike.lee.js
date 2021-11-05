0f4ca83c0d653e53e6fa1f4b0ba72978ca92f51f
mike lee <git@iskitz.com>
2021-11-05 03:36:51 -0700

    identity: updated [me:my] mike.lee

--- team/mike.lee.js
+++ team/mike.lee.js
@@ -5,13 +5,15 @@
     , as: ['im.0']
     , by:  'mike.lee@ionify.net'
     , on:  -7.20180327
-    , to:  -7.20211103
-    , at:  -0.014
+    , to:  -7.20211105
+    , at:  -0.015
     , is: "Mike Lee's Identity Module"
     }
     ,
   is:
     [ 'Michael Lee'         , 'Mike Lee'
+    , '🇬🇾.mike.👨🏾‍💻'           , '🇬🇾.mike.🇺🇸'
+    , 'mike.lee@baramita'
     , 'mike.lee@ionify'     , 'mike.lee@ionified'
     , 'mike.lee@iskitz'     , 'mike-lee@jsperf'
     , 'mikeleepro@linkedin' , 'mikeleepro@yahoo'
@@ -26,11 +28,11 @@
     ]
     ,
   author:
-    [ 'ionified', 'ionify', 'jsonxd'  , 'ajile', 'jspackaging'
+    [ 'ionified', 'ionify', 'jsonxd'  , 'ajile' , 'jspackaging'
     , 'ask.say' ,'wakatta', 'anemojii'
-    , 'aeon'    ,  'aesop', 'caption' , 'dion' , 'ion'   , 'im'
-    , 'jem'     ,   'jeni', 'jet'     , 'jewel', 'lion'  , 'obi'
-    , 'oj'      ,  'orion', 'storie'  , 'webi'
+    , 'aeon'    ,  'aesop', 'caption' , 'dion'  , 'ion'   , 'im'
+    , 'jem'     ,   'jeni', 'jet'     , 'jewel' , 'lion'  , 'obi'
+    , 'oj'      ,  'orion', 'savi'    , 'storie', 'webi'
     , 'webOS 3.x Calendar Fixes'      , 'webOS 3.x Calendar'
     , 'DataHub', 'hollyhouk.com'      , 'Canon Authorized Send'
     , 'MEAP Remote Management System' , 'iDesign@iskitz'
@@ -42,7 +44,8 @@
     ]
     ,
   member:
-    [ 'netflix', '/dev/color', 'khan academy', 'mozilla', 'amazon'
+    [ 'baramita'
+    , 'netflix', '/dev/color', 'khan academy', 'mozilla', 'amazon'
     , 'hp'     , 'palm'      , 'ionify'      , 'canon'  , 'sybase'
     , 'bsu'    , 'nsbe'      , 'wpi'         , 'hamilton standard'
     , 'inroads', 'emsep'     , 'strive'
