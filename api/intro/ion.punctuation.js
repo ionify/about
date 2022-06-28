c23d4eef720d8ecd384755c888e456005c2d8219
Mike Lee <iskitz@users.noreply.github.com>
2022-06-27 21:25:14 -0700

    docs & code: exhibition of ecmascript's ion-compatible punctuation! :nerd_face:

---
+++ api/intro/ion.punctuation.js
@@ -0,0 +1,43 @@
+;
+~
+{ re:
+    { id:  'ion.punctuation@about.ionify'
+    , of: ['core','conduit','punctuation']
+    , as: ['exhibition']
+    , by: ['mike.👨🏾‍💻.lee']
+    , on: -8.20171111
+    , to: -7.20220627
+    , at:  0.1
+    , is: "an exhibition of ecmascript ion punctuation that combines fifteen "
+        + "lions with seventeen ion-compatible bitwise, arithmetic, relational "
+        + "& equality punctuation operators"
+        ,
+      go:
+        { help: 'https://github.com/ionify/ionify/issues'
+        , plan: 'https://github.com/ionify/ionify/projects/1?fullscreen=true'
+        , code: 'https://github.com/ionify/avout/blob/public/api/intro/ion.punctuation.js'
+        , test: 'https://github.com/ionify/ionify/blob/public/ions/on.ion.punctuation.test.js'
+        , deal: 'https://deal.ionify.net/'
+        , join: 'https://github.com/ionify/about/tree/public/team'
+        , seek: 'https://api.ionify.net/'
+        },
+      we:
+        [ "were ... "
+        , "must update with new ion-compatible ecmascript operators like **"
+        , "will ... "
+        , "want ... "
+        , "like ... "
+        ]
+    },
+
+  do:function
+  exhibit()
+    { ~  /example/  -  /punctuation/
+      ^  ["aesop"]  &  /  stories  /  >>  {are:"ions"}
+      *  ["these"]  +  /expressions/  <<  {are:"too" }
+      /  ["write"]  %  /them as you/  |   {see:"fit" }
+      >  ["using"]  >= /  any of   /  <=  [  "these" ]
+      <     17      == / compatible/  !=  "operators!"
+    }
+}
+;
