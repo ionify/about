deecf706f442fc4b9a30fb3b9726b536a7eb69ce
Mike Lee <iskitz@users.noreply.github.com>
2022-11-04 07:47:37 -0700

    docs & code: +re@ specification instruction & whitespace reformatting
    why
    : was missing actual re@ instruction
    : had re@ members without re@!
    : reformatting helps comprehension

--- ions/xml.stories.js
+++ ions/xml.stories.js
@@ -1,45 +1,49 @@
 ;
 ~
-{ id:  'xml.stories@ions.iskitz.net'
-, as:  {xml:0.002, tomes:0.001, stories:0.002}
-, by:  'mike.👨🏾‍💻.lee@ionify.net'
-, in:  'san-jose.california.us.earth'
-, on:  -7.202003200700
-, to:  -7.202211040707
-, at:  +0.07
-, is: [`an exploration of xml via
-        tomes: textual, object-markup,
-        expressions & stories: simple,
-        tome-recognition, evaluations
-       `,
-       `showing that xml can be
-        transformed to, expressed,
-        and evaluated, as tomes by:
-       `,
-       `inserting $'s between
-        xml tags and /'s before
-        each tag's name & closing
-        > bracket
-       `,
-       `replacing leading <'s &
-        trailing >'s with /'s
-       `,
-       `wrapping xml text content
-        with a -[ prefix and ]-
-        postfix.
-       `
-      ],
-  we: [ "must ..."
-      , "will ..."
-      , "want ..."
-      , "love ..."
-      , "like this for non-web js environments like node.js"
-      , "know that web views can already import xml to js"
-      , "wish ..."
-      , "cant ..."
-      , "wont ..."
-      ]
-, do: $=1
+{ re:
+    { id: 'xml.stories@ions.iskitz.net'
+    , as: {xml:0.002,tomes:0.001,stories:0.002}
+    , by: 'mike.👨🏾‍💻.lee@ionify.net'
+    , in: 'san-jose.california.us.earth'
+    , on: -7.202003200700
+    , to: -7.202211040737
+    , at: +0.07
+    , is:
+        [`an exploration of xml via
+          tomes: textual, object-markup,
+          expressions & stories: simple,
+          tome-recognition, evaluations
+         `,
+         `showing that xml can be
+          transformed to, expressed,
+          and evaluated, as tomes by:
+         `,
+         `inserting $'s between
+          xml tags and /'s before
+          each tag's name & closing
+          > bracket
+         `,
+         `replacing leading <'s &
+          trailing >'s with /'s
+         `,
+         `wrapping xml text content
+          with a -[ prefix and ]-
+          postfix.
+         `
+        ],
+      we:
+        [ "must ..."
+        , "will ..."
+        , "want ..."
+        , "love ..."
+        , "like this for non-web js environments like node.js"
+        , "know that web views can already import xml to js"
+        , "wish ..."
+        , "cant ..."
+        , "wont ..."
+        ]
+    },
+  do: $=1
 }
 
 $ </these/>
