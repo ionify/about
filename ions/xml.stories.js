56bdba65f0987722639eeee5219316c08d6a9cc5
Mike Lee <iskitz@users.noreply.github.com>
2022-11-04 07:26:11 -0700

    docs & code: xml.stories: +tomes: textual, object-markup, expressions
    why
    : terminology helps comprehension
    : show tome variation & applications

--- ions/xml.stories.js
+++ ions/xml.stories.js
@@ -1,46 +1,57 @@
 ;
 ~
 { id:  'xml.stories@ions.iskitz.net'
+, as:  {xml:0.002, tomes:0.001, stories:0.002}
 , by:  'mike.👨🏾‍💻.lee@ionify.net'
 , in:  'san-jose.california.us.earth'
 , on:  -7.202003200700
-, to:  -7.202211031200
-, at:  +0.06
-, as: ['xml','stories']
-, is:  `an exploration of xml as
-        stories`
-, it: [`shows that xml can be
-        transformed to and
-        ingested as stories by:`
-      ,`inserting $'s between
-        xml tags, and /'s before
+, to:  -7.202211040707
+, at:  +0.07
+, is: [`an exploration of xml via
+        tomes: textual, object-markup,
+        expressions & stories: simple,
+        tome-recognition, evaluations
+       `,
+       `showing that xml can be
+        transformed to, expressed,
+        and evaluated, as tomes by:
+       `,
+       `inserting $'s between
+        xml tags and /'s before
         each tag's name & closing
-        > bracket!`
-      ,`replacing leading <'s &
-        trailing >'s with /'s!`
-      ]
-, we: [`must ...`
-      ,`will ...`
-      ,`want ...`
-      ,`love ...`
-      ,`like this for non-web js environments like node.js`
-      ,`know that web views can already import xml to js`
-      ,`wish ...`
-      ,`cant ...`
-      ,`wont ...`
+        > bracket
+       `,
+       `replacing leading <'s &
+        trailing >'s with /'s
+       `,
+       `wrapping xml text content
+        with a -[ prefix and ]-
+        postfix.
+       `
+      ],
+  we: [ "must ..."
+      , "will ..."
+      , "want ..."
+      , "love ..."
+      , "like this for non-web js environments like node.js"
+      , "know that web views can already import xml to js"
+      , "wish ..."
+      , "cant ..."
+      , "wont ..."
       ]
 , do: $=1
 }
 
 $ </these/>
-$   </look like="xml"/>
+$    </look like="xml"/>
 $ </or/>
-$   </xhtml/>
+$    </xhtml/>
 $ </or/>
-$   </html data-attribute="🤓"/>
-$   </tags/>
+$    </html data-attribute="🤓"/>
+$       -["arrayed, textual, observable-markup, expression"]-
+$    </tags/>
 $ </but/>
-$ </these are="xml😉stories" by="mike👨🏾‍💻lee"/>
+$ </these are="xml😉tomes" by="mike👨🏾‍💻lee"/>
 $
 
 ; /these//
@@ -49,7 +60,8 @@ $
      /xhtml//
   /or//
      /html data-attribute=🤓//
+        -["arrayed, textual, observable-markup, expression"]-
      /tags//
   /but//
-  /these are=xml😉stories by=mike👨🏾‍💻lee/
+  /these are=xml😉tomes by=mike👨🏾‍💻lee/
 ;
