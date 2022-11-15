2ef67f4cd94316c3963ebb82ff7fa17952c4d106
iskitz <git@iskitz.com>
2022-11-15 00:12:49 -0800

    docs & code: intro: + proof of ion supplanting jsonp
    more
    : oj: observable-json response from api.geonames
    : shows ion's ~({"json":"data"}); technique

---
+++ api/intro/ion.replaces.jsonp.proof.js
@@ -0,0 +1,6 @@
+~({"status": {
+  "message": "the hourly limit of 1000 credits for demo has been exceeded. Please use an application specific account. Do not use the demo account for your application.",
+  "value": 19
+}});
+
+~/from as recently as -7.202110250421: http://api.geonames.org/countryCodeJSON?formatted=true&lat=4.5&lng=59.5&username=demo&style=full&callback=~/
