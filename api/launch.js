94804adf08d79437bc36c80397dc708c549f584e
Mike Lee <iskitz@users.noreply.github.com>
2023-01-09 19:17:37 -0800

    docs: layout & launch: ionify's organization & initiation proposals
    all
    : re.by acknowledges :bowing_man:🏾‍♂️ יהוה 🤲🏾
    : re.by acknowledges mike:guyana::man:🏾‍:computer::us:lee
    : re.on+to: as elaborated +| arithmetic instantiation@s with
    postfixed-calendar for elaborated & prefixed-calendar &
    postfixed-timezone for arithmetic.

--- api/launch.js
+++ api/launch.js
@@ -1,15 +1,74 @@
 ;
 ~
 { re:
-    { id: "launch@info.ionify"
+    { id: 'launch@info.ionify'
     , is: "ionify's launch notes"
-    , by: "mike.lee@iskitz"
-    , in: "san-jose.california.usa.earth"
-    , at: "2017.04.02-07...04.01-07"
+    , by: ['🙇🏾‍♂️ יהוה 🤲🏾','mike.🇬🇾👨🏾‍💻🇺🇸.lee@iskitz']
+    , in: 'silicon-valley.california.usa.earth'
+    , on: -2.20170401707
+    , to: -1.57831004408
+    , at: -0.002
     },
 
   LAUNCH:
-    [`
+    [`web
+        -> Object.prototype.valueOf
+                .with.host=
+                .with.web =
+                        .own= web
+                        .get= web.getScript
+                        .get: get.js
+
+        -> get
+              -> ~get = .with.host.get
+              -> delete .with.host & .with.web
+
+              -> .with.ion.pending =
+                      { 0: .with.host.re.id, .with.host.re.id: .with.host.own
+                      , 1:       this.re.id,       this.re.id:  get
+                      }
+
+              ->  get : ionify  = on.ion.js
+                      : sion    = linker    = find.link.share
+                      : next.id
+                      : id
+                      : error, errors, logger, ...
+
+              -> sion -> keep   .with.ion.pending
+                      -> delete .with
+                      -> link self
+                      -> share own share() & space()
+                      -> link kept.pending
+                      ->   ~  kept.pending
+    `,
+
+    `web
+        -> on.ion
+                ->  on:no
+                        ->  @ionify .senses
+                        ->  @*    ~ionified
+
+                ->  ~get
+
+                ->  next.id
+
+                    id  ->  on:* ion.re.id <= ~next.id
+
+                    linker
+                        ->  linker.with.*
+                        ->  @ionify: share, space
+                        ->  on:*:sion ion.with.*: ion.with.our[domain] <= ion.re.id
+
+                    get
+                    no
+                    error
+                    errors
+                    logger
+                        -> ~error
+                        -> @*: debug, error, info, log, warn
+
+    `,
+    `
         .----------------------------.        .---------.
       1 | host: web.js, node.js, etc |      5 | ions/on |
         '----------------------------'        '---------'
@@ -39,6 +98,297 @@
                                                       }
                                                  }
 
-    `]
+    `],
+
+  'ROLES & FLOW -7.20220627...-7.20221102|2022110297000100':
+    [ `
+      application:
+
+          location-of-ionify/web.js[?parameter=value&parameter=value,...]
+          location-of-ionify/web.js
+          location-of/ionify.web.js[?parameter=value&parameter=value,...]
+          location-of/ionify.web.js
+          location-of/newrenamed.js
+          location-of/newrenamedext
+
+      connection:web@
+
+          conduit.base:observation
+            senses  obi  from self until conduit@
+            records launch history until conduit@
+            bridges future to past-state
+
+          acquisition
+            sense
+
+              setup:configuration.flow:initialization.preserve
+                👍🏾  stop
+                🤷🏾‍♂️  start
+                    sensed(obi) obi.re.as === 'configuration@' && obi.definitions:
+                      web.of.own.setup.definitions = obi.definitions
+                    get:'./connection@.ions.js'
+                👎🏾  adapt
+
+              obi
+                setup.pion
+                  1+ member.depth.1 is id@*
+                  setup available for inspection immediately
+                  setup.flow.adapt + no.setup~get's & tbd.implementation~get's
+
+                setup.concatenation
+                  1+ obi.sensed as caller.same
+                  setup available for inspection subsequently
+                  setup.flow.adapt + no.setup~get's & tbd.implementation~get's
+
+                setup.realization
+                  1+ obi.sensed as caller.different
+                  setup available for inspection subsequently
+                  setup.flow.start + setup~get's & implementation~get's
+
+                setup.as:definition@s
+
+                  which:groups:collections
+
+                    core:foundations@
+                      is everything needed
+                      location:
+                        ['./es/', '//server.doma.in/path/to/', ...]
+                      composition:
+                        ['definitions@','conventions@','information@','operations@','applications@']
+                      language:
+                        language to request for its ions e.g. es:core,base,next,#,name
+                        'es.core'
+                        language unspecified applies 1st configuration@ language sensed
+
+                    definitions@
+                      is everything identified
+                      location:
+                        ['./es/as/', '//server...', '...']
+                      composition:
+                        ['definitions@','conventions@','information@','operations@','applications@']
+
+                    conventions@
+                      are core & persistent actions & information
+                      with no location means id is path or connection@ resolution
+                      composition:
+                        ['re@','id@','of@','as@']
+
+                    observation@
+                      are conduit actions
+                      composition:
+                        [ 'ion@','error@','aeon@','aesop@','action@','jedi@'
+                        , 'jem@','oj@','stories@'
+                        ]
+
+                    information@
+                      are supportive information
+                      composition:
+                        ['list.map@','queue@','simple.map@']
+
+                    operations@
+                      are supportive actions
+                      composition:
+                        ['resolution@','transcription@','configuration@']
+
+                    resolution@
+                      are id resolution actions
+                      composition:
+                        ['compatibility@','is.type@','find@','use@']
+
+                    compatibility@
+                      are compatibility actions
+                      composition:
+                        ['es@']
+
+                    transcription@
+                      are recording actions
+                      composition:
+                        ['logger@']
+
+                    configuration@
+                      are configuration actions
+                      composition:
+                        ['on.ions@']
+
+                    applications@
+                      are language & constructs apis
+                      composition:
+                        ['on@','do@','get@','types@']
+
+                  where:sions:location
+
+                    keep each as map.member.depth.1
+                    id@s location resolution
+                      which:sions
+                      where:paths:uris:uniform-resource-identification
+
+      `,
+      `
+      ionify's composition
+
+          foundations@ionify
+
+              definition@
+                  types: definition@,      tools:    operation@,
+                         convention@,   conduits: observation@,
+                  api:  application@
+
+              information@
+                  list.map@ + queue@ + simple.map@
+
+              operation@
+
+                  resolution@
+                      compatibility@
+                          es@ es.next-core-base-past-#-name.#
+
+                      is.type: ion.ified + find + use
+
+                  transcription@  logger@
+                  configuration@  on.ions@
+
+                  next.id
+                  cion@: cion ({next,id}||{find.in}||{warn})
+                         ☝🏾concealed invocation: not part of ion flow; normal js evaluation
+                         | excluded from ion stacktrace's unless on error | intentional
+                         + simpler & less: convoluted, verbose & onerous challenge-solving
+
+              convention@
+                  re@ + id@ + of@ + as@
+
+              observation@
+                  ion@      :on.object
+                  error@    :on.error   + errors@
+                  aeon@     :on.array   + aesop@
+                  action@   :on.function
+                  jedi@     :on.json    + jem@ + oj@
+                  stories@  :on.regexp
+
+              application@
+                  on@ <-- no@ --> do@ + get@ + {types@ <- configurable as read-only namespaces}
+      `,
+      `ionify's initialization roles & flows:
+
+        * sions' members are only accessible to each other except what's in its of@s which's available per its @if
+        * can do auto-memory-cleanup via sensation [map of] receipt & release, notifications to & ion responses
+          * attempt to free sensation@ host of sensation@ memory since it has a new long-living purposeful host
+          * tell sensation@ host when all its sensation@s are forgotten
+
+        * transcription@s
+        *   note: expected things done
+        *   spot: known  challenges with max detail, and suspected challenges with  see note suggestion
+        *   warn: solved challenges, and suspected challenges with                  see spot suggestion
+        *   fail: failed challenges with max detail &                               see spot suggestion
+
+        * get@s
+
+          * only invoke ions after their relations are known & ~get.got,
+            then after ~get'ing & receiving them, invoke them in
+            ¿ what order ~get or receive fifo or filo?
+
+          * auto-sense receipt & fail:+timeout, transcribe & tell, if it has a ~get.then
+          * queue of@'s in invocation order, ~get all async, but invoke in order, like proven js in web browsers 🙇🏾‍♂️👨🏾‍💻🤓
+          * initialization always has precedence because...it's initializing, i.e. doing the first actions! 😅
+          * web@ script 404 => es.downgrade + retry, until es.past then fail:¿halt:false? simpler if halts
+          * always check if request succeeded earlier & only redo if explicitly asked via new:true
+
+        * sensation@ does   auto~on:sensation,sensation: as:sensation.ion.of.own.on.sensation if exists else ignores
+        * sensation@.ions   implement or declare ion.of.own.on:all,others,sensation[.specific]() or they'll be ignored
+        * connection@s    receive a with@ instance with known environment@s preset within it
+                            create  a rudimentary conduit that senses ions until replaced by conduit@
+                            define  a set of default ionify locations
+        * initialization@s  receive a configuration@ instance with preset settings & categories.resolve()s it can set
+        *    acquisition@   ion@ sets own.get to es.next:? es.core:import es.base:dom es.past:?
+        *    acquisition@s  are expected to have an ion.of.own.on.get.queue() which must understand & do
+
+                              { put: [values]: after        + before:all    <- warn@ for before initialization@s
+                              , cut: [values]: after:value  + before:value  <- warn@ for affect initialization@s
+                              }
+
+        * acquisition@s     receive a get@ who's ~get'rs & their .async, .type & .then they can set
+        * observation@s     can have an ion.of.own.on.observation.type() for all their on@:type declarations
+        * observation@s     receive conduit instances with .enable,disable,preserve()
+        *  convention@s     are expected to have an on@:*[,if@:[]] declaration
+        * ...
+
+      connection: environment@
+
+         0.declare: as: initialization@, connection@ & acquisition@
+         1.declare: environment@ is conduit.ionify.initialization
+         2.own.get: environment@ & foundations@
+                      ||  conduit, context, get@ (the acquisition sensation composition), configuration
+                     ☝🏾👉🏾  own.get will infer all these based on environment@'s re.as definitions:types
+         3.  queue: environment@
+         4.  index: environment@'s  of@ relations          👈🏾(queue auto-does this)
+         5.  queue: environment@'s  of@ relations at front 👈🏾(queue auto-does this)
+         6. remove: earliest  DUPLICATE relations in queue 👈🏾(queue auto-does these)☝🏾
+         7.resolve: all in queue per configuration@ or connection@'s: uri or local paths: /es/as/
+                    ☝🏾👉🏾 web:connection@ can do like ajile.legacy
+                    ☝🏾👉🏾 can use ionify@'s tests.html *-src technique to configure alternates
+                                 src: /web.js
+                              as-src: /es/as/foundations
+                              overrides default in web.js, overidden by configuration@s
+                    ☝🏾👉🏾 succeeds: keeps type@, uri, retries & relation@s to help other resolution@s
+                    ☝🏾👉🏾 failures: auto-retry resolution@  if new configuration@s since last
+                    ☝🏾👉🏾 failures: with found resolution@s auto-retry with es:next-core-base-past
+         8.  sense: of.own.on.get.done
+         9.request: all in queue simultaneously unless told otherwise
+        ...
+        10.  index: foundations@'s  of@ relations
+        11.  queue: foundations@  & its relations at front ,but behind initialization
+        10. remove: earliest  DUPLICATE relations in queue 👈🏾(queue auto-does these)☝🏾
+         9.  sense: of.own.on.get.done
+        10.request: all in queue simultaneously unless told otherwise
+        11.receive: of.own.on.get.done ({go:[]{order:get:[],got:[]}, no:[]{like .go}})
+        12. invoke: of.own.on.get.done.go.get list 👈🏾(get@ auto-does this, unless ~get:vet:true)
+
+      foundation: foundations@
+
+        10.declare: as:pion configuration: packaged ionify ions.js'
+            ~
+            {'foundations@ionify.net':
+                          {re:
+                            {as:
+                              ['pion',     'configuration',
+                               'definition',   'operation',
+                               'convention','observation',
+                               'application'
+                            }}],
+               'definition@ionify': {re:{}},
+                      '...@ionify': {re:{}},
+              'observation@ionify': {re:{}}
+            , 'application@ionify': {re:{}}
+            }
+
+      ionification: ion@
+
+       // .declare: as: acquisition,    definition, initialization
+                      , observation,  connection,     convention
+                      , application
+
+      observation: conduit@
+
+         7.invoke: initialization@      sensation
+         8.invoke: connection@        sensation since conduit will attach itself to connection & that to sions
+         9.invoke: observation@        sensation
+        10.insert: conduit@ at the beginning of the queue
+        13.invoke: conduit@.ionify.initialization  which is environment@
+        14. sense: environment@ as initialization@
+        15. adopt: environment@  & initialization@ queue
+        16. sense: environments ~get acquisition sensation composition & keep it
+        17.  ~get: foundations@ionify
+
+        react to initialization's queue sharing
+
+      interpretation: context@
+
+         9.declare as context & connection
+        10.invoke context sensation convention
+        11.react to context sensation reate .with context, known groups & set them within context
+        10.invoke obi context convention sensation
+        11.invoke connection sensation
+    `
+    ]
+
 }
 ;
--- api/layout.js
+++ api/layout.js
@@ -1,23 +1,87 @@
 ;
 ~
 { re:
-    { id: "layout@info.ionify"
+    { id: 'layout@info.ionify'
     , is: "ionify's directory layout"
-    , by: "mike.lee@iskitz"
-    , in: "san-jose.california.usa.earth"
-    , at: "2017.04.15-07...03.31-07"
-        ,
-      stories:
+    , by: ['🙇🏾‍♂️ יהוה 🤲🏾','mike.🇬🇾👨🏾‍💻🇺🇸.lee@iskitz']
+    , in: 'silicon-valley.california.usa.earth'
+    , on: -2.20170331607
+    , to: -1.57831004408
+    , at: -0.002
+    , stories:
         [ "todo: ..."
         , /note: .test alongside ions develops test-making habit/
         , /note: .test alongside ions highlights missing tests/
         , /note: .re's alongside ions develops doc-writing habit/
         ]
-    }
+    },
 
-    ,
   is:
-    [`
+    [`proposal: -7.20220627...-7.20221102|2022110297000100
+
+      ionify.js
+      : re
+        : *.md      <- readme, license, origin, vision & lingo
+        : notes.js  <- undecided observations
+        : plans.js
+        : seek.html <- ~get's /ion/ions.js then processes & renders all their re's++
+      : es
+        : #         <- specific version's   functioning implementations
+        : name      <- specific flavour's   functioning implementations
+        : next      <- future   completely  functioning            ions 🤩 ions.js
+        : core      <- current  completely  functioning    compositions 😌 ions.js
+        : base      <- earliest completely  functioning    compositions 🥳 ions.js
+        : past      <- earliest  partially  functioning    compositions 🤓 ions.js
+        :           ^............. defines  functioning   augmentations 🤗 ions.js
+        : ions.js
+        : apis
+          : ion*.js
+          : ion*.am.js
+          : ion*.im.js
+          : ion*.test.js
+        : conduits
+          : ion*.js
+          : ion*.am.js
+          : ion*.im.js
+          : ion*.test.js
+        : conventions
+          : ion*.js
+          : ion*.am.js
+          : ion*.im.js
+          : ion*.test.js
+        : as|types
+          : ion*.js
+          : ion*.am.js
+          : ion*.im.js
+          : ion*.test.js
+        : tools
+          : ion*.js
+          : ion*.am.js
+          : ion*.im.js
+          : ion*.test.js
+          : ion*.*.html   <-  auto~get's appropriate js from html's name like ajile.legacy  👈🏾👇🏾
+      : css  <-------------- /es/-compatible directory | folder structure for  automated resolution
+      : dom  <-------------- ^
+      : ion  <-------------- |
+      : node <-------------- v
+      : web  <-------------- /es/-compatible directory | folder structure for  automated resolution
+      :                   ^..ions.js defines connection-specific configuration
+      :                       overidden by /connection@.ions.js
+      :                       language
+      :                         es: next, core, base     <-- ¿future migration problem as each shifts?
+      :                           , #
+      :                           , actionscript, coffeescript, javascript, jscript, typescript, vbscript, *
+      : any.js  <-----------  by any applications relying on the environments' automated resolution
+      : css.js
+      : css.ions.js <-------  configuration@ pion ...
+      : dom.js
+      : dom.ions.js <-------  configuration@ pion ...
+      : web.js
+      : web.ions.js <-------  configuration@: pion or as@ definition@s or any configuration@ set
+      :node.js
+     `
+    ,`proposal: 2017.04.15-07...03.31-07
+
       /api/
         : ionify.node.0.1.js
         : ionify.node.js
